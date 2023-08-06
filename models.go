4ef5dca2721d2aa6a1f2b91fcd4ed3b544f3cca3
Shawn Koong <shawnkoong@gmail.com>
2023-08-05 22:47:17 -0700

    adding Path methods and CoordinateBearing

--- models.go
+++ models.go
@@ -34,18 +34,45 @@ type Section struct {
 }
 
 type Trip struct {
-	Id                   string             `json:"id"`
-	Stops                map[string]float64 `json:"stops"`
-	DelayedTime          float64            `json:"delayed_time"`
-	ScheduledDiscrepancy float64            `json:"scheduled_discrepancy"`
-	IsDelayed            bool               `json:"is_delayed"`
-	IsAssigned           bool               `json:"is_assigned"`
-	LastStopMade         string             `json:"last_stop_made"`
+	Id                   string           `json:"id"`
+	Stops                map[string]int64 `json:"stops"`
+	DelayedTime          float64          `json:"delayed_time"`
+	ScheduledDiscrepancy float64          `json:"scheduled_discrepancy"`
+	IsDelayed            bool             `json:"is_delayed"`
+	IsAssigned           bool             `json:"is_assigned"`
+	LastStopMade         string           `json:"last_stop_made"`
 }
 
 type Path struct {
 	PathName string
 	Points   []Point
+}
+
+func (p *Path) appendToFront(point Point) {
+	p.Points = append([]Point{point}, p.Points...)
+}
+
+func (p *Path) appendToEnd(point Point) {
+	p.Points = append(p.Points, point)
+}
+
+// appends all points into p.Points except the first point from path
+func (p *Path) appendAll(path *Path) {
+	points := path.Points[1:]
+	p.Points = append(p.Points, points...)
+}
+
+func (p *Path) updateName(newName string) {
+	p.PathName = newName
+}
+
+func (p *Path) length() int {
+	return len(p.Points)
+}
+
+func (p *Path) getPointAtProgress(progress float64) CoordinateBearing {
+	points := p.Points
+	// TODO
 }
 
 type Point struct {
@@ -64,4 +91,10 @@ type ArrivalTime struct {
 	Time       int64
 	IsDelayed  bool
 	IsAssigned bool
+}
+
+type CoordinateBearing struct {
+	Longitude float64
+	Latitude  float64
+	Bearing   float32
 }
